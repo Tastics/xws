69034c9ba9d746bba993b34a220cb4ec544afb2d
cendawei <280842458@qq.com>
2024-06-26 00:33:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Nav from './components/Nav.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+    <!-- Navbar -->
+    <Nav></Nav>
+    <RouterView />
+    <Footer></Footer>
+</template>
+
+<style scoped></style>
